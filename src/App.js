7a78f0c6243ef675cc9d3382be6fab2dd3f771ec
ch04937 <97hernandez.c@gmail.com>
2019-08-05 12:54:08 -0700

    starting project created constructor and todo data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import ReactDom from 'react-dom';
+
+const todoData = [
+  {
+    item: 'Learn setState',
+    id: '1',
+    completed: false
+  }, 
+  {
+    item: 'Style my Todo List',
+    id: '2',
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      item: 'Todo List: MVP',
+      toDo: todoData
+    };
+  }
   render() {
     return (
       <div>
