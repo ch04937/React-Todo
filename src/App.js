1e4f95b20136f8a762e85a96fbb8e335cf00728c
ch04937 <97hernandez.c@gmail.com>
2019-08-05 13:49:22 -0700

    added button and clear todo button needs functionalty

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import TodoForm from './components/TodoComponents/TodoForm'
+import TodoList from './components/TodoComponents/TodoList';
 
 const todoData = [
   {
     item: 'Learn setState',
-    id: '1',
+    id: 1,
     completed: false
   }, 
   {
     item: 'Style my Todo List',
-    id: '2',
+    id: 2,
     completed: false
   }
 ]
@@ -22,13 +24,55 @@ class App extends React.Component {
     super();
     this.state = {
       item: 'Todo List: MVP',
-      toDo: todoData
+      todo: todoData
     };
   }
+  toggleItem = id => {
+    console.log(id);
+    this.setState({
+      todo: this.state.todo.map(item => {
+        if (item.id === id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+  addItem = itemName => {
+    const newItem = {
+      item: itemName, 
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      todo: [...this.state.todo, newItem]
+    });
+  };
+  clearPurchased = () => {
+    this.setState({
+      todo: this.state.todo.filter(item => !item.completed)
+    });
+  };
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <p>{this.state.item}</p>
+        <ul>
+          <li>
+          </li>
+        </ul>
+        <TodoForm addItem={this.addItem} />
+        <TodoList
+            todo={this.state.todo}
+            toggleItem={this.toggleItem}
+            />
+
+
       </div>
     );
   }
